1d506168b5a4ad9772f6b439017894ddb010069c
Sean Chang <schang@health2sync.com>
2021-01-26 17:29:56 +0800

    commit

--- app/genVideoRecording.js
+++ app/genVideoRecording.js
@@ -9,22 +9,60 @@ client.video.rooms(roomSid)
   .then(recordings => {
     console.log('list recordings success')
 
-    const videoRecordings = []
+    // 排序，使用者影像不分順序，先進會議先顯示，share screen總是在最後
+    let videoRecordings = []
+    let shareScreenRecordings = []
     recordings.forEach(recording => {
       if (recording.type === 'video') {
-        videoRecordings.push(recording.sid)
+        // share screen
+        if (recording.trackName.startsWith('share-screen')) {
+          shareScreenRecordings.push(recording.sid)
+        } else {
+          videoRecordings.push(recording.sid)
+        }
       }
     })
 
-    // 製作影片
-    client.video.compositions.create({
-      roomSid,
-      audioSources: '*',
-      videoLayout: {
-        single: {
+    if (shareScreenRecordings.length > 0) {
+      videoRecordings = videoRecordings.concat(shareScreenRecordings)
+    }
+    // 排序結束
+
+    // 定義layout
+    let videoLayout = {}
+
+    // 有share screen
+    if (shareScreenRecordings.length > 0) {
+      videoLayout = {
+        grid: {
+          max_columns: 2,
+          max_rows: 2,
+          x_pos: 12,
+          y_pos: 12,
           video_sources: videoRecordings
         }
-      },
+      }
+    } else { // 無share screen
+      videoLayout = {
+        grid: {
+          max_columns: 2,
+          max_rows: 1,
+          x_pos: 12,
+          y_pos: 180,
+          width: 1256,
+          height: 360,
+          video_sources: videoRecordings
+        }
+      }
+    }
+    // 定義layout結束
+
+    // 製作影片
+    client.video.compositions.create({
+      roomSid: roomSid,
+      resolution: '1280x720',
+      videoLayout: videoLayout,
+      audioSources: '*',
       trim: true,
       format: 'mp4',
       statusCallback: null
