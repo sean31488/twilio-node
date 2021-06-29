83dffc3af2b2623d57684abad7c1aa25119d71e9
Sean Chang <schang@health2sync.com>
2021-06-29 16:03:57 +0800

    adjust videoLayout

--- app/genVideoRecording.js
+++ app/genVideoRecording.js
@@ -9,57 +9,37 @@ client.video.rooms(roomSid)
   .then(recordings => {
     console.log('list recordings success')
 
-    // 分離人像及分享螢幕視訊軌
-    let hasShareScreen = false
-    let avatarRecordings = []
-    recordings.forEach(recording => {
-      if (recording.type === 'video') {
-        // share screen
-        if (recording.trackName.startsWith('share-screen')) {
-          hasShareScreen = true
-        } else {
-          avatarRecordings.push(recording.sid)
-        }
-      }
-    })
-
     // 定義layout
-    let videoLayout = {}
-
-    // 有share screen
-    if (hasShareScreen) {
-      videoLayout = {
-        main: {
-          width: 960,
-          height: 540,
-          x_pos: 308,
-          y_pos: 90,
-          video_sources: ['share-screen_*'],
-        },
-        column: {
-          width: 284,
-          height: 540,
-          x_pos: 12,
-          y_pos: 90,
-          max_columns: 1,
-          reuse: 'show_newest',
-          video_sources: avatarRecordings // 僅有人像視訊軌
-        }
-      }
-    } else { // 無share screen
-      videoLayout = {
-        grid: {
-          max_columns: 2,
-          max_rows: 1,
-          x_pos: 12,
-          y_pos: 180,
-          width: 1256,
-          height: 360,
-          video_sources: avatarRecordings
-        }
+    let videoLayout = {
+      main: {
+        width: 840,
+        height: 472,
+        x_pos: 430,
+        y_pos: 131,
+        video_sources: ['*'],
+        video_sources_excluded: ['hcp_*', 'share-screen_*'],
+      },
+      column: {
+        width: 416,
+        height: 234,
+        x_pos: 10,
+        y_pos: 131,
+        max_columns: 1,
+        max_rows: 1,
+        reuse: 'show_newest',
+        video_sources: ['hcp_*'],
+      },
+      row: {
+        width: 416,
+        height: 234,
+        x_pos: 10,
+        y_pos: 369,
+        max_columns: 1,
+        max_rows: 1,
+        reuse: 'show_newest',
+        video_sources: ['share-screen_*'],
       }
     }
-    // 定義layout結束
 
     // 製作影片
     client.video.compositions.create({
